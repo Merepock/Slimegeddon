e8df5548672a5f9490bff5745465e5e8ac874607
Merepock <douglas.wil801@gmail.com>
2021-12-10 17:54:35 +0000

    Wipe Powerup rarity increased

--- Assets/Scripts/SpawnPowerup.cs
+++ Assets/Scripts/SpawnPowerup.cs
@@ -21,13 +21,13 @@ public class SpawnPowerup : MonoBehaviour
             if (spawnTimer <= 0)
             {
                 int chance = Random.Range(0, 20);
-                if (chance % 4 == 0)
-                {
-                    powerUp = Instantiate(ghostPowerup, new Vector2(Random.Range(-9, 9), Random.Range(-5, 5)), transform.rotation);
-                }
-                else if (chance % 3 == 0)
+                if (chance % 10 == 0)
                 {
                     powerUp = Instantiate(wipePowerup, new Vector2(Random.Range(-9, 9), Random.Range(-5, 5)), transform.rotation);
+                }
+                else if (chance % 4 == 0)
+                {
+                    powerUp = Instantiate(ghostPowerup, new Vector2(Random.Range(-9, 9), Random.Range(-5, 5)), transform.rotation);
                 }
                 else
                 {
